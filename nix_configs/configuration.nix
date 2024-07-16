9fc53bd53dd1ef9aa36bc73afc8d25c942338612
Zachary Jones <17038716+1sudo@users.noreply.github.com>
2024-07-16 01:58:11 -0500

    Update configuration.nix

--- nix_configs/configuration.nix
+++ nix_configs/configuration.nix
@@ -1,161 +1,196 @@
-# https://search.nixos.org/options
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
-
+let
+  unstableTarball = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  unstable = import unstableTarball { config = config.nixpkgs.config; };
+in
 {
-  nixpkgs.config.allowUnfree = true;
-
   imports =
-    [
+    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./neovim/main.nix
     ];
 
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos";
+  boot.initrd.luks.devices."luks-f238a6c2-46b9-4b9d-a6be-783f2ac61529".device = "/dev/disk/by-uuid/f238a6c2-46b9-4b9d-a6be-783f2ac61529";
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    useXkbConfig = true; # use xkb.options in tty.
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
-  systemd.user.targets.sway-session = {
-    description = "Sway compositor session";
-    documentation = [ "man:systemd.special(7)" ];
-    bindsTo = [ "graphical-session.target" ];
-    wants = [ "graphical-session-pre.target" ];
-    after = [ "graphical-session-pre.target" ];
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = false;
+  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.defaultSession = "plasma";
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
   };
 
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    git
-    discord
-    discordo
-    tmux
-    screen
-    pavucontrol
-    parsec-bin
-    obs-studio
-    webcord
-    parsec-bin
-    gnome.nautilus
-    bitwarden
-  ];
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-  hardware.opengl = {
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      vulkan-loader
-      vulkan-validation-layers
-      vulkan-extension-layer
-      amdvlk
-    ];
-    extraPackages32 = with pkgs; [
-      driversi686Linux.amdvlk
-    ];
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
-  environment.variables.AMD_VULKAN_ICD = "RADV";
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
-  environment.shellInit = ''
-    source $HOME/.bashrc
-  '';
+  virtualisation.docker.enable = true;
+  users.extraGroups.docker.members = [ "zac" ];
 
-  programs.bash = {
-    shellAliases = {
-      firefox="nohup firefox > /dev/null 2>&1 &";
-      discord="nohup discord > /dev/null 2>&1 &";
-      parsec="nohup parsecd > /dev/null 2>&1 &";
-    };
-  };
-
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-    extraPackages = with pkgs; [
-      dmenu
-      swaylock
-      swayidle
-      xwayland
-      xwaylandvideobridge
-      grim # screenshot functionality
-      slurp # screenshot functionality
-      wl-clipboard # copy/paste from stdin/stdout
-      mako # notification system for sway
-      kanshi
-      wf-recorder
-    ];
-    extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-      export _JAVA_AWT_WM_NONREPARENTING=1
-      export MOZ_ENABLE_WAYLAND=1
-    '';
-  };
-
-  systemd.user.services.kanshi = {
-    description = "Kanshi output autoconfig";
-    wantedBy = [ "graphical-session.target" ];
-    partOf = ["graphical-session.target" ];
-    environment = { XDG_CONFIG_HOME="/home/zac/.conf"; };
-    serviceConfig = {
-      ExecStart = ''
-      ${pkgs.kanshi}/bin/kanshi
-      '';
-      RestartSec = 5;
-      Restart = "always";
-    };
-  };
-
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  services.xserver.displayManager.defaultSession = "sway";
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.libinput.enable = true;
-
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  boot.supportedFilesystems = [ "nfs" ];
-  services.rpcbind.enable = true;
-
-  fileSystems."/data0" = {
-    device = "192.168.1.151:/mnt/user/data0";
-    fsType = "nfs";
-  };
-
-  fileSystems."/emby1" = {
-    device = "192.168.1.151:/mnt/user/emby1";
-    fsType = "nfs";
-  };
-
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zac = {
     isNormalUser = true;
-    uid = 1000;
-    home = "/home/zac";
-    createHome = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    description = "zac";
+    extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      firefox
-      terminator
+      kdePackages.kate
+    #  thunderbird
     ];
   };
-  
-  # Do not edit
-  system.stateVersion = "23.11"; 
-}
 
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.variables = { EDITOR = "vim"; };
+
+  environment.systemPackages = with pkgs; [
+    (pkgs.neovim.override {
+        vimAlias = true;
+        configure = {
+	 packages.myPlugins = with pkgs.vimPlugins; {
+            start = [ vim-nix ];
+	   opt = [];
+	 };
+          customRC = ''
+            set relativenumber
+            set nocompatible
+            set backspace=indent,eol,start
+          '';
+        };
+      }
+    )
+    unstable.vesktop
+    wget
+    discord
+    parsec-bin
+    kitty
+    nerdfonts
+    docker-compose
+    protonup-qt
+    vscode
+    git
+    pavucontrol
+    alsa-utils
+    vlc
+    clang_18
+    clang-tools_18
+    ninja
+    obs-studio
+    go
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        golang.go
+        ms-vscode.cpptools-extension-pack
+        jdinhlife.gruvbox
+        ms-azuretools.vscode-docker
+        ms-vscode-remote.remote-ssh
+        llvm-vs-code-extensions.vscode-clangd
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "remote-ssh-edit";
+          publisher = "ms-vscode-remote";
+          version = "0.47.2";
+          sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+        }
+      ];
+    })
+  ];
+
+  programs.steam.enable=true;
+
+  # Required for VSCode under Wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
