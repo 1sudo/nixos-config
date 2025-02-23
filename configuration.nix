9306556b874c146206ace2fc8497fe1dabc11cd2
Zachary Jones <zjones89@outlook.com>
2025-02-22 21:24:29 -0600

    Updated nix config

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
 
 {
   imports =
@@ -7,12 +11,24 @@
       ./kitty.nix
     ];
 
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
   networking.networkmanager.enable = true;
+
+  # Set your time zone.
   time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -27,36 +43,62 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = false;
   services.desktopManager.plasma6.enable = true;
 
+  services.desktopManager.cosmic.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
+
+  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
+  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  hardware.pulseaudio.enable = false;
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
-  virtualisation.docker.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zac = {
     isNormalUser = true;
     description = "Zac";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
   };
 
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
     discord-canary
     helix
@@ -68,6 +110,7 @@
     parsec-bin
     unzip
     zip
+    plasma-panel-colorizer
     protonup-qt
     go
     htop
@@ -77,6 +120,10 @@
     obs-studio
     bottles
     meld
+    gimp
+    inkscape
+    gtk3
+    gtk4
     postman
     transmission_4-qt
     docker-compose
@@ -84,24 +131,58 @@
     talosctl
     kubectl
     kubernetes-helm
+    jq
+    envsubst
+    kubecolor
+    git-lfs
+    tree
   ];
 
-  services.transmission.settings.download-dir = "${config.services.transmission.home}/Downloads";
+  services.transmission.settings = {
+    download-dir = "${config.services.transmission.home}/Downloads";
+  };
+
+  # environment.sessionVariables = {
+  #   LD_LIBRARY_PATH = lib.makeLibraryPath [
+  #     pkgs.gtk3
+  #     pkgs.pango
+  #     pkgs.cairo
+  #     pkgs.glib
+  #     pkgs.alsa-lib
+  #     pkgs.libpulseaudio
+  #     "${pkgs.chromium}/lib"
+  #   ];
+  # };
 
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
   };
 
-  programs.firefox.enable = true;
-  programs.steam.enable = true;
-  programs.nix-ld.enable = true;
+  programs.steam = {
+    enable = true;
+  };
 
+  programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     stdenv.cc.cc.lib  # libstdc++
     zlib
     glibc
   ];
+
+  networking.extraHosts =
+    ''
+      192.168.1.224 longhorn.warpsec.com
+      192.168.1.224 wiki2.cuemu.com
+      192.168.1.224 stats.warpsec.com
+    '';
+
+  security.pam.loginLimits = [
+    { domain = "*"; item = "nofile"; type = "-"; value = "65536"; }
+    { domain = "*"; item = "memlock"; type = "-"; value = "65536"; }
+  ];
+  systemd.user.extraConfig = "DefaultLimitNOFILE=65536";
+  systemd.extraConfig = "DefaultLimitNOFILE=65536";
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -116,4 +197,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
+
 }
